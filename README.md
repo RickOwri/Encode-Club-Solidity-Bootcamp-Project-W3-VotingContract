96f5c8abfbe5e8aad7a3115bdaab15df6e1cb0e6
Mickael <mickael.rutembya@gmail.com>
2023-05-20 18:17:06 +0200

    beforemass

--- README.md
+++ README.md
@@ -1,1 +1,80 @@
-# week03
+# Instructions : 
+## week03
+Form groups of 3 to 5 students
+Complete the contracts together
+Develop and run scripts for “TokenizedBallot.sol” within your group to give voting tokens, delegating voting power, casting votes, checking vote power and querying results
+Write a report with each function execution and the transaction hash, if successful, or the revert reason, if failed
+Share your code in a github repo in the submission form
+
+## Scripts
+- voting tokens
+- delegating voting power
+- casting votes
+- checking vote power
+- querying results
+
+## command
+
+# Homework
+
+## Project 1 - With interface in TokenizedBallot.ts
+
+
+### Voting tokens
+
+clear; yarn run ts-node --files scripts/1_ERC20Votes.ts ghost in the shell
+
+### ERCVotes
+
+### TokenizedBallot
+
+### Give voting power 
+
+### Delegating Voting Power
+
+
+---
+
+## Project 1 - Without interface
+
+### Voting tokens
+### ERCVotes
+ERCVotes deployed
+
+clear; yarn run ts-node --files scripts/1_ERC20Votes.ts ghost in the shell
+
+Connected to the address 0xc8e653ea3F2245C640506659180a3F2a2189AfB3
+Alchemy API key length:32
+Balance is 487835679923968732 WEI
+Connected to the Block number 3492303
+The contract was deployed at address 0x0b651023bD3589665F9ed981c5EeBa96dec86EaB at block 3492304
+
+### TokenizedBallot
+clear; yarn run ts-node --files scripts/DeployTokenizedBallot.ts ghost in the shell
+
+Connected to the address 0xc8e653ea3F2245C640506659180a3F2a2189AfB3
+Alchemy API key length:32
+Connected to the Block number 3492326
+Balance is 481852902892060588 WEI
+Proposals:
+Proposal N. 1: ghost
+Proposal N. 2: in
+Proposal N. 3: the
+Proposal N. 4: shell
+0x0b651023bD3589665F9ed981c5EeBa96dec86EaB
+entre
+0x74edD52064C089731DE9b39eAa011a5b7a7D13B6
+0x74edD52064C089731DE9b39eAa011a5b7a7D13B6
+Contract deployed at address 0x74edD52064C089731DE9b39eAa011a5b7a7D13B6
+
+### Give voting power 
+clear; yarn run ts-node --files scripts/GiveVotingPower.ts
+
+
+
+
+### Delegating Voting Power
+
+
+
+yarn hardhat run scripts/deployBallot.ts
--- contracts/TokenizedBallot.sol
+++ contracts/TokenizedBallot.sol
@@ -1,14 +1,11 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.0 <0.9.0;
 
-import "./MyERC20Votes.sol";
-
-
 interface IMyVoteToken {
     function getPastVotes(address, uint256) external view returns (uint256);
 }
 
-contract Ballot {
+contract TokenizedBallot {
 
     struct Proposal {
         bytes32 name;   
@@ -19,37 +16,35 @@ contract Ballot {
     Proposal[] public proposals;
     uint256 public targetBlockNumber;
 
-    mapping(address => uint256) public votingPowerSpent;
+    mapping(address => uint256 amount) votingPowerSpent;
 
-    constructor(
-            bytes32[] memory proposalNames,
-            address _tokenContract,
-            uint256 _targetBlockNumber                
-        ) {
-        tokenContract = IMyVoteToken(_tokenContract);
-        targetBlockNumber = block.number;
-        targetBlockNumber = _targetBlockNumber;
-        for (uint256 i = 0; i < proposalNames.length; i++) {
-            proposals.push(Proposal({name: proposalNames[i], voteCount: 0}));
+    constructor(bytes32[] memory proposalNames, address _tokenContract, uint256 _targetBlockNumber) {
+
+        for (uint i = 0; i < proposalNames.length; i++) {
+     
+            proposals.push(Proposal({
+                name: proposalNames[i],
+                voteCount: 0
+            }));
         }
+        targetBlockNumber = _targetBlockNumber;
+        tokenContract = IMyVoteToken(_tokenContract);
     }
 
     function vote(uint256 proposal, uint256 amount) external {
-        require(
-            tokenContract.getPastVotes(msg.sender, targetBlockNumber) >= amount
-        );
-        // TODO: Account voting power
-        // TODO: Update votes for the proposals
-        proposals[proposal].voteCount += amount;    
+
+        require(votingPower(msg.sender) >= amount, "TokenizedBallot: Trying to vote more than allowed");
+        votingPowerSpent[msg.sender] += amount;
+        proposals[proposal].voteCount += amount;
     }
 
-    function votingPower(address account) public view returns (uint256) {
-        return
-            tokenContract.getPastVotes(account, targetBlockNumber) -
-            votingPowerSpent[account];
+    function votingPower(address account) public view returns (uint256){
+        return tokenContract.getPastVotes(account, targetBlockNumber) -  votingPowerSpent[account];
     }
 
-    function winningProposal() public view returns (uint winningProposal_) {
+    function winningProposal() public view
+            returns (uint winningProposal_)
+    {
         uint winningVoteCount = 0;
         for (uint p = 0; p < proposals.length; p++) {
             if (proposals[p].voteCount > winningVoteCount) {
@@ -59,7 +54,10 @@ contract Ballot {
         }
     }
 
-    function winnerName() external view returns (bytes32 winnerName_) {
+    function winnerName() external view
+            returns (bytes32 winnerName_)
+    {
         winnerName_ = proposals[winningProposal()].name;
     }
+
 }
